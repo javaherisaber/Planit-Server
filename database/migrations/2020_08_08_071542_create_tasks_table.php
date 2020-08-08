64f8cd182d4b20e2bbbec50bda53d3e10a1bf4d8
Mahdi <m.javaherisaber@gmail.com>
2020-08-08 13:31:25 +0430

    Create migrations for projects and tasks

---
+++ database/migrations/2020_08_08_071542_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id')->nullable()->constrained();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamp('due_at')->nullable()->comment('تخمین زمان مقرر انجام دادن');
+            $table->timestamp('completed_at')->nullable()->comment('زمان تکمیل شدن');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
